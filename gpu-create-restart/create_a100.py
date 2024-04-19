3bbb471a328e2aaef56f66b8f85d9ec589b4a62f
Your Name <you@example.com>
2024-04-19 12:10:46 -0400

    adding extra fields to the instance request

--- gpu-create-restart/create_a100.py
+++ gpu-create-restart/create_a100.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python
-#pip install google-cloud-notebooks
+# This is a simple example of how to request an instance with a GPU using the notebook API. May contain bugs, for reference purposes only.
+
 from google.cloud import notebooks_v1
 from google.api_core import exceptions as core_exceptions
 import datetime
-
-client = notebooks_v1.NotebookServiceClient()
-
-request = {
-    'parent': 'projects/YOU_GCP_PROJECT_NAME_HERE/locations/us-central1-a',
-    'instance_id': 'pytorch2-a100',
-    'instance': {
-        'install_gpu_driver': True,
-        'machine_type': 'a2-highgpu-1g',
-        'vm_image': {
-            'image_name': "pytorch-2-0-gpu-notebooks-v20230925-debian-11-py310",
-            'project': "deeplearning-platform-release"
-        }
-    }
-}
 
 def create_notebook_instance(request):
     try: 
@@ -26,15 +12,41 @@ def create_notebook_instance(request):
         print("Waiting for operation to complete...")
         response = operation.result()
         return response
-    except core_exceptions.ServiceUnavailable:
+    except core_exceptions.ServiceUnavailable as e:
+        print(e)
         print("Unavailable, trying again...")
         return None
 
-status = None
-while(status == None):
-    status = create_notebook_instance(request)
-    print(status)
+if __name__ == '__main__':
 
+    # Ref: https://cloud.google.com/python/docs/reference/notebooks/latest/google.cloud.notebooks_v1.services.notebook_service.NotebookServiceClient
+    client = notebooks_v1.NotebookServiceClient()
 
+    # Ref:. https://cloud.google.com/python/docs/reference/notebooks/latest/google.cloud.notebooks_v1.types.CreateInstanceRequest
+    request = {
+        'parent': 'projects/YOU_PROJECT_ID/locations/us-central1-a',
+        'instance_id': 'pytorch2-a100',
+        'instance': { # ref.: https://cloud.google.com/python/docs/reference/notebooks/latest/google.cloud.notebooks_v1.types.Instance
+            'install_gpu_driver': True,
+            'service_account': "000000000000-compute@developer.gserviceaccount.com",
+            'tags': ['allow-ssh'],
+            'network': 'projects/YOU_PROJECT_ID/global/networks/default', #vpc name: 'default'
+            'subnet': 'projects/YOU_PROJECT_ID/regions/us-central1/subnetworks/default', #subnetwork name 'default'
+            'machine_type': 'a2-highgpu-1g',
+            'boot_disk_size_gb': 256,
+            'boot_disk_type': notebooks_v1.Instance.DiskType.PD_SSD,
+            'data_disk_size_gb': 512,
+            'data_disk_type': notebooks_v1.Instance.DiskType.PD_SSD,
+            'vm_image': { #Ref. https://cloud.google.com/deep-learning-vm/docs/images#listing-versions 
+                'image_name': "pytorch-2-2-cu121-notebooks-v20240319-debian-11-py310",
+                'project': "deeplearning-platform-release"
+            }
+        }
+    }
 
+    # Loop
+    status = None
+    while(status == None):
+        status = create_notebook_instance(request)
+        print(status)
 
